44e9b8c4ed14017a0160eb9bcc705984ef323308
Madeleine Fischer <mlfischer@alumni.stanford.edu>
2026-07-08 08:52:47 -0400

    medicare A and B components

--- clean_data.py
+++ clean_data.py
@@ -4,13 +4,29 @@ import pandas as pd
 import numpy as np
 
 dir = "C:/Users/MadeleineFischer/OneDrive - Committee for a Responsible Federal Budget/Documents/Medicare"
-survey_data = pd.read_csv(f"{dir}/Medicare Current Beneficiary Survey - Survey File/SFPUF2023_Data/sfpuf2023_1_fall.csv")
-cost_data= pd.read_csv(f"{dir}/Medicare Current Beneficiary Survey - Cost Supplement/CSPUF2023_Data/cspuf2023.csv")
 
-# Check for matches of participant IDs between the two datasets
-participant_ids_survey = set(survey_data['PUF_ID'])
-participant_ids_cost = set(cost_data['PUF_ID'])
-matching_ids = participant_ids_survey.intersection(participant_ids_cost)
+# Import survey data and clean
+# survey_data = pd.read_csv(f"{dir}/Medicare Current Beneficiary Survey - Survey File/SFPUF2023_Data/sfpuf2023_1_fall.csv")
+cost_data = pd.read_csv(f"{dir}/Medicare Current Beneficiary Survey - Cost Supplement/CSPUF2023_Data/cspuf2023.csv")
 
-if len(matching_ids) > 0:
-    pass
+# Format into DataFrame
+cost_supplement = pd.DataFrame(cost_data)
+
+# Part A = inpatient + share(home health) + 
+cost_supplement['medicare_partA'] = cost_supplement['PAMTIP'] + cost_supplement['PAMTHH']
+
+# Part B = outpatient + share(home health) + medical provider + dental + vision + hearing
+cost_supplement['medicare_partB'] = cost_supplement['PAMTMP'] + cost_supplement['PAMTDU'] + cost_supplement['PAMTVU'] + cost_supplement['PAMTHU'] + cost_supplement['PAMTDU']
+
+# Calculate effective coinsurance rates for Part A and Part B
+
+
+
+
+
+# INS_D_PRIVNUM: has private insurance
+# INS_D_PVESI : employer-sponsored private insurance
+# INS_D_PVSELF: self-purchased private insurance
+# ACC_MCDRSOON: Usually go to Dr as soon as feel bad 
+
+# Use the cost data to apply CBO formula to figure out spending changes
